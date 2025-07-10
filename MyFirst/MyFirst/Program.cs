05a5be9f7268c36c3bd68f2f4251e5e80acf329b
MakaRonish <ronishmakaju12345@gmail.com>
2025-07-10 00:29:07 -0400

    prac

---
+++ MyFirst/MyFirst/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age = 23;
+            //int vol;
+            //vol = 90;
+            //Console.WriteLine(vol);
+
+            //long bigNumber = 90000000000;
+            //Console.WriteLine(bigNumber);
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+            //Console.WriteLine(long.MinValue);
+
+
+
+            //float precision = 5.0001f;
+            //Console.WriteLine(precision);
+
+            //int family, fage, fmember;
+            //int gg = 90, wp = 90, fg = 99;
+
+
+
+            //string name = "Ronish";
+            //char letter = 'R';
+
+            //Console.WriteLine("Your Name is ", name+"and letter ",letter);
+
+
+            //string textage = "99";
+            //int rage = Convert.ToInt32(textage);
+            //Console.WriteLine(rage);
+
+            //bool value = true;
+            //bool isTrue = false;
+
+
+            // rage ++;
+            //Console.WriteLine(rage);
+
+            //const int vat = 14;
+            //int balance = 99;
+            //Console.WriteLine($"Total amount is{(balance + (vat * balance) / 100d)}");
+
+
+            //string t1 = "";
+
+
+            int num = 100;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("is even");
+
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ Practice/first.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            int vol;
+            vol = 90;
+            Console.WriteLine(vol);
+
+            long bigNumber = 90000000000;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MinValue);
+
+
+
+            float precision = 5.0001f;
+            Console.WriteLine(precision);
+
+            int family, fage, fmember;
+            int gg = 90, wp = 90, fg = 99;
+
+        }
+    }
+}
