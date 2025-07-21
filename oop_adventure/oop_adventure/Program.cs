92760cf662cbe0c859802a9719134ed0fca7a183
MakaRonish <ronishmakaju12345@gmail.com>
2025-07-20 23:18:32 -0400

    oopadventure

---
+++ oop_adventure/oop_adventure/Program.cs
@@ -0,0 +1,20 @@
+﻿using OOPAdventure;
+namespace oop_adventure
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello What is your name: ");
+            string name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "NoName";
+            }
+
+            var player = new Player(name);
+            Console.Write($"Hello, {player.Name}");
+
+        }
+    }
+}
